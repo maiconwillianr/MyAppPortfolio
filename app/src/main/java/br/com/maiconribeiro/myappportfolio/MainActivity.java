fd9a262e0e0580606f6932785a951c7873399031
maiconwillianribeiro <maiconwillianr@gmail.com>
2016-09-02 19:17:22 -0300

    Button Click

--- app/src/main/java/br/com/maiconribeiro/myappportfolio/MainActivity.java
+++ app/src/main/java/br/com/maiconribeiro/myappportfolio/MainActivity.java
@@ -1,13 +1,64 @@
 package br.com.maiconribeiro.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my Popular Movies app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my stock Hawk app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my Built it Bigger app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my Make our app Material app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my Go Ubiquitous app", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "This Button will launch my Capstone app", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
